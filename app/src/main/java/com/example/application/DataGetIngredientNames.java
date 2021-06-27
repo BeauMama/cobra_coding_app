65e702f1467b628c480b724497e1e71eeb685393
Abram Ephraim <eph20001@byui.edu>
2021-06-26 21:24:08 -0600

    Changed database to singleton.

--- app/src/main/java/com/example/application/DataGetIngredientNames.java
+++ app/src/main/java/com/example/application/DataGetIngredientNames.java
@@ -1,48 +1,20 @@
 package com.example.application;
 
-import android.app.Activity;
-
-import androidx.recyclerview.widget.RecyclerView;
-
-import java.lang.ref.WeakReference;
+import java.security.InvalidParameterException;
 import java.util.List;
+import java.util.concurrent.Callable;
 
-public class DataGetIngredientNames implements Runnable {
+public class DataGetIngredientNames implements Callable<List<String>> {
 
     private DataDao dataDao;
-    private List<String> ingredientNames;
-    private WeakReference<Activity> activity;
 
-    public DataGetIngredientNames(Activity activity, DataDao dataDao, List<String> ingredientNames) {
-        this.activity = new WeakReference<>(activity);
+    public DataGetIngredientNames(DataDao dataDao) {
         this.dataDao = dataDao;
-        this.ingredientNames = ingredientNames;
+
     }
 
-    @Override
-    public void run() {
-        ingredientNames = dataDao.getIngredientNames();
-
-        if (ingredientNames.size() > 0) {
-            System.out.println("Ingredient List");
-            for (String ingredient : ingredientNames) {
-                System.out.println(ingredient);
-            }
-
-            /*
-            Activity mainActivity = this.activity.get();
-            if (mainActivity != null) {
-                mainActivity.runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        RecyclerView recyclerView = mainActivity.findViewById(R.id.recipeList);
-                        ViewRecipeListAdapter viewRecipeListAdapter = new ViewRecipeListAdapter(recipes);
-                        recyclerView.setAdapter(viewRecipeListAdapter);
-                    }
-                });
-            }
-
-             */
-        }
+    public List<String> call() throws InvalidParameterException {
+        return dataDao.getIngredientNames();
     }
 }
+
